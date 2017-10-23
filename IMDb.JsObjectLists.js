246fc76c60b1677ded5b4da371122ea720b48c1a
Adán Mauri Ungaro <adan.mauri@gmail.com>
2017-10-23 01:22:32 -0300

    AutoCosmos added. W3C validations

---
+++ IMDb.JsObjectLists.js
@@ -0,0 +1,15 @@
+class AutoCosmosJsObjectLists extends JsObjectLists {
+	constructor(url){
+		
+		var url = url || "https://www.autocosmos.com.ar/search?q=" ;
+		var elementSelector = {
+			title: "//div[contains(@class, 'version')]//a[contains(@data-profile, 'clasificados')]//h3/text()",
+			url: "//div[contains(@class, 'version')]//a[contains(@data-profile, 'clasificados')]/@href",
+			img: "//div[contains(@class, 'version')]//a[contains(@data-profile, 'clasificados')]//img/@data-defer-src",
+			price: "//div[contains(@class, 'version')]//a[contains(@data-profile, 'clasificados')]//span[@class='precio']/strong/text()"
+		};
+		
+		super(url, elementSelector);		
+	}
+}
+
